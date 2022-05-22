88945e77ea44942f374c8458cce7ea97d7f74fc4
Safx <safxdev@gmail.com>
2022-05-23 00:13:03 +0900

    update for apollo router

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for apollo-router.
 GH_REPO="https://github.com/apollographql/router"
 TOOL_NAME="apollo-router"
 TOOL_TEST="router --version"
@@ -31,18 +30,17 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if apollo-router has other means of determining installable versions.
   list_github_tags
 }
 
 download_release() {
-  local version filename url
+  local version filename url arch platform
   version="$1"
   filename="$2"
+  arch="$3"
+  platform="$4"
 
-  # TODO: Adapt the release URL convention for apollo-router
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="$GH_REPO/releases/download/v${version}/router-${version}-${arch}-${platform}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -58,10 +56,9 @@ install_version() {
   fi
 
   (
-    mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    mkdir -p "$install_path/bin"
+    cp "$ASDF_DOWNLOAD_PATH"/router "$install_path/bin"
 
-    # TODO: Asert apollo-router executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
